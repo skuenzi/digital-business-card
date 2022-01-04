2ce8d1b623762645264181484840e5ebc4729b0b
sara.kuenzi <48669509+skuenzi24@users.noreply.github.com>
2022-01-03 23:42:03 -0700

    basic layout and components

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+
+function Header () {
+    return (
+        <header>
+            <img />
+            <h1>Sara Kuenzi</h1>
+            <p>Frontend Developer</p>
+            <div>
+                <small><a>skuenzi.github.io</a></small>
+                <div>
+                    <button>Email</button>
+                    <button>LinkedIn</button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/Components/Socials.js
@@ -0,0 +1,15 @@
+import {FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare} from 'react-icons/fa'
+
+function Socials () {
+    return (
+        <footer>
+            <button><FaTwitterSquare /></button>
+            <button><FaFacebookSquare /></button>
+            <button><FaInstagramSquare /></button>
+            <button><FaGithubSquare /></button>
+
+        </footer>
+    )
+}
+
+export default Socials

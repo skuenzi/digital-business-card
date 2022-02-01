5530a9f9eb0cbab39366f2c83079eefd39407bc9
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-02-01 14:53:05 -0700

    added functionality to socials buttons

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -3,10 +3,9 @@ import {FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare} fr
 function Socials () {
     return (
         <footer className='socials'>
-            <button className='socials-btn'><FaTwitterSquare className='socials-icon'/></button>
-            <button className='socials-btn'><FaFacebookSquare className='socials-icon'/></button>
-            <button className='socials-btn'><FaInstagramSquare className='socials-icon'/></button>
-            <button className='socials-btn'><FaGithubSquare className='socials-icon'/></button>
+            <button className='socials-btn' onClick={() => window.open('https://www.facebook.com/thekwenz24')}><FaFacebookSquare className='socials-icon'/></button>
+            <button className='socials-btn' onClick={() => window.open('https://www.instagram.com/thekwenz24/')}><FaInstagramSquare className='socials-icon'/></button>
+            <button className='socials-btn'onClick={() => window.open('https://github.com/skuenzi')}><FaGithubSquare className='socials-icon'/></button>
         </footer>
     )
 }
